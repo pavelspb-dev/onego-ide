5d4dd0da2580071dd05bc891f57b489448f87aa5
Pavel <pavelspb-dev@mail.ru>
2026-09-11 19:31:12 +0300

    Update onegobios.py

--- onegobios.py
+++ onegobios.py
@@ -1,12 +1,16 @@
 
 class BIOS_Error(ValueError):
+    """BIOS find problem"""
     pass
 
 class BIOS:
     def __init__(self) -> None:
+        """Initializes BIOS"""
         pass
 
-    def start(self, cpu) -> None:
+    def start(self, cpu, write: bool = False) -> None:
+        """Starts BIOS. If write = True, writes BIOS messages"""
+
         self.cpu = cpu
 
         if not callable(getattr(self.cpu, "write", None)):
@@ -27,12 +31,18 @@ class BIOS:
         if not callable(getattr(self.cpu.ram, "write", None)):
             raise BIOS_Error("Ошибка BIOS: ОЗУ не поддерживает метод записи")
 
-#        self.cpu.write("OnegoBIOS запущен!\nИнициализация ЖД...\n")
-        self.cpu.ssd.init()
- #       self.cpu.write("ЖД инициализирован.\nЗагрузка данных в ОЗУ...\n")
+        if write:
+            self.cpu.write("OnegoBIOS запущен!\nИнициализация ЖД...\n")
 
+        self.cpu.ssd.init()
+
+        if write:
+            self.cpu.write("ЖД инициализирован.\nЗагрузка данных в ОЗУ...\n")
+
+        # Writing data from SSD to RAM
         for addr in range(0, 11384):
             data = self.cpu.ssd.read(addr)
             self.cpu.ram.write(addr + 5000, data)
 
-#        self.cpu.write("Готово.\nOnegoBIOS успешно завершил работу, передаётся управление ЦП.\n")
+        if write:
+            self.cpu.write("Готово.\nOnegoBIOS успешно завершил работу, передаётся управление ЦП.\n")
